20415d4ac1f1293b7258c58a1593be2b917f60b0
me@dirkz.com <me@dirkz.com@5aa2a760-743d-11de-9207-fb4211ab666b>
2009-08-31 17:45:13 +0000

    branch for 1.1

---
+++ branches/1.1/Classes/ItemAmountViewController.h
@@ -0,0 +1,42 @@
+//
+//  ItemAmountViewController.h
+//  iNetHack
+//
+//  Created by dirk on 8/22/09.
+//  Copyright 2009 Dirk Zimmermann. All rights reserved.
+//
+
+//  This file is part of iNetHack.
+//
+//  iNetHack is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, version 2 of the License only.
+//
+//  iNetHack is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with iNetHack.  If not, see <http://www.gnu.org/licenses/>.
+
+#import <UIKit/UIKit.h>
+
+@class NethackMenuItem, Window;
+
+@interface ItemAmountViewController : UIViewController {
+
+	IBOutlet UIImageView *imageView;
+	IBOutlet UILabel *itemTextLabel;
+	IBOutlet UILabel *itemDetailTextLabel;
+	IBOutlet UILabel *amountTextLabel;
+	IBOutlet UISlider *amountSlider;
+	IBOutlet UIButton *dropButton;
+	
+	Window *menuWindow;
+	
+}
+
+@property (nonatomic, assign) Window *menuWindow;
+
+@end
