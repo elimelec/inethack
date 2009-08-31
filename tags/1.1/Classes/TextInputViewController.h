814ef23e4b932cc592bd9c557fb965faccbd9a56
me@dirkz.com <me@dirkz.com@5aa2a760-743d-11de-9207-fb4211ab666b>
2009-08-31 17:46:06 +0000

    tag for 1.1

---
+++ tags/1.1/Classes/TextInputViewController.h
@@ -0,0 +1,52 @@
+//
+//  TextInputViewController.h
+//  iNetHack
+//
+//  Created by dirk on 7/3/09.
+//  Copyright 2009 Dirk Zimmermann. All rights reserved.
+//
+
+//  This file is part of iNetHack.
+//
+//  iNetHack is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, version 2 of the License only.
+//
+//  iNetHack is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with iNetHack.  If not, see <http://www.gnu.org/licenses/>.
+
+#import <UIKit/UIKit.h>
+
+
+@interface TextInputViewController : UIViewController <UITextFieldDelegate> {
+	
+	IBOutlet UITextField *tf;
+	IBOutlet UILabel *label;
+	NSString *prompt;
+	NSString *text;
+	BOOL numerical;
+	UIReturnKeyType returnKeyType;
+	
+	id target;
+	SEL action;
+
+	NSCondition *condition;
+	BOOL returned;
+	
+	// avoid reentrance problems (issue 66)
+	int reentered;
+}
+
+@property (nonatomic, retain) NSString *prompt;
+@property (nonatomic, retain) NSString *text;
+@property (nonatomic, assign) UIReturnKeyType returnKeyType;
+@property (nonatomic, assign) BOOL numerical;
+@property (nonatomic, retain) id target;
+@property (nonatomic, assign) SEL action;
+@property (nonatomic, retain) NSCondition *condition;
+@end
