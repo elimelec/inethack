3e90afbd54aad2c4cafc41ae5fd7d6dad0681570
me@dirkz.com <me@dirkz.com@5aa2a760-743d-11de-9207-fb4211ab666b>
2009-11-21 12:52:14 +0000

    tag for 1.3-beta2

---
+++ tags/1.3-beta2/Classes/TileSet.m
@@ -0,0 +1,83 @@
+//
+//  TileSet.m
+//  iNetHack
+//
+//  Created by dirk on 7/12/09.
+//  Copyright 2009 Dirk Zimmermann. All rights reserved.
+//
+
+//  This file is part of iNetHack.
+//
+//  iNetHack is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, version 2 of the License only.
+//
+//  iNetHack is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with iNetHack.  If not, see <http://www.gnu.org/licenses/>.
+
+#import "TileSet.h"
+
+extern short glyph2tile[];
+
+static TileSet *_instance = nil;
+
+@implementation TileSet
+
++ (TileSet *) instance {
+	return _instance;
+}
+
++ (int) glyphToTileIndex:(int)g {
+	return glyph2tile[g];
+}
+
+- (id) initWithImage:(UIImage *)image tileSize:(CGSize)ts {
+	if (self = [super init]) {
+		tileSize = ts;
+		if (image) {
+			CGImageRef base = image.CGImage;
+			int x = CGImageGetWidth(base) / tileSize.width;
+			int y = CGImageGetHeight(base) / tileSize.height;
+			numImages = x*y;
+			images = malloc(numImages * sizeof(CGImageRef));
+			int index = 0;
+			for (int j = 0; j < y; ++j) {
+				for (int i = 0; i < x; ++i) {
+					CGRect r = CGRectMake(i*tileSize.width, j*tileSize.height, tileSize.width, tileSize.height);
+					images[index++] = CGImageRetain(CGImageCreateWithImageInRect(base, r));
+				}
+			}
+		}
+		if (!_instance) {
+			_instance = self;
+		}
+	}
+	return self;
+}
+
+- (CGImageRef) imageAt:(int)i {
+	return images[i];
+}
+
+- (CGImageRef) imageForGlyph:(int)g atX:(int)x y:(int)y {
+	return [self imageAt:[TileSet glyphToTileIndex:g]];
+}
+
+- (CGImageRef) imageForGlyph:(int)g {
+	return [self imageForGlyph:g atX:0 y:0];
+}
+
+- (void) dealloc {
+	for (int i = 0; i < numImages; ++i) {
+		CGImageRelease(images[i]);
+	}
+	free(images);
+	[super dealloc];
+}
+
+@end
